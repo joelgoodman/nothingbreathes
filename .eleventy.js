e9c314be133a226e886b276eaca9863f2a8d4ccd
Joel Goodman <joel@braverymedia.co>
2025-03-13 22:10:31 -0700

    First post

---
+++ .eleventy.js
@@ -0,0 +1,124 @@
+import { DateTime } from "luxon";
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+import blogTools from "eleventy-plugin-blog-tools";
+import htmlmin from "html-minifier";
+import CleanCSS from "clean-css";
+import markdownIt from "markdown-it";
+
+const TIME_ZONE = "America/Los_Angeles";
+
+export default function(eleventyConfig) {
+  eleventyConfig.addPlugin(blogTools);
+  eleventyConfig.setDataDeepMerge(true);
+  eleventyConfig.addWatchTarget("./src/assets/css/nb.css");
+
+  	eleventyConfig.addPlugin(feedPlugin, {
+		type: "atom", // or "rss", "json"
+		outputPath: "/feed.xml",
+		collection: {
+			name: "posts", // iterate over `collections.posts`
+			limit: 0, // 0 means no limit
+		},
+		metadata: {
+			language: "en",
+			title: "nothing breathes / joel goodman",
+			subtitle: "A personal blog by Joel Goodman. Rewinding it to '06 or so.",
+			base: "https://nothingbreathes.online/",
+			author: {
+				name: "Joel Goodman",
+				email: "hello@joelgoodman.co",
+			},
+		},
+	});
+
+  eleventyConfig.addDateParsing(function (dateValue) {
+		let localDate;
+		if (dateValue instanceof Date) {
+			// and YAML
+			localDate = DateTime.fromJSDate(dateValue, { zone: "utc" }).setZone(
+				TIME_ZONE,
+				{ keepLocalTime: true }
+			);
+		} else if (typeof dateValue === "string") {
+			localDate = DateTime.fromISO(dateValue, { zone: TIME_ZONE });
+		}
+		if (localDate?.isValid === false) {
+			throw new Error(
+				`Invalid \`date\` value (${dateValue}) is invalid for ${this.page.inputPath}: ${localDate.invalidReason}`
+			);
+		}
+		return localDate;
+  });
+
+  /* Date filters */
+  // Human readable
+  eleventyConfig.addFilter("readableDate", dateValue => {
+    return DateTime.fromJSDate(dateValue, {zone: TIME_ZONE}).toFormat("dd LLL yyyy");
+  });
+
+  eleventyConfig.addFilter("yearDate", dateValue => {
+    return DateTime.fromJSDate(dateValue, {zone: TIME_ZONE}).toFormat("yyyy");
+  });
+
+  // Machine Readable https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter('htmlDateString', (dateValue) => {
+    return DateTime.fromJSDate(dateValue, {zone: TIME_ZONE}).toFormat('yyyy-LL-dd');
+  });
+
+  /* Minification filters */
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+
+  eleventyConfig.addTransform("htmlmin", function(content) {
+    if( this.outputPath.endsWith(".html") ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+
+    return content;
+  });
+
+ function getPosts(collectionApi) {
+		return collectionApi
+			.getFilteredByGlob("./src/posts/**/*.md")
+			.reverse()
+			.filter(function (item) {
+				return !!item.data.permalink;
+			});
+ }
+
+ eleventyConfig.addCollection("posts", function (collection) {
+		return getPosts(collection);
+ });
+
+  /* Passthrough for assets */
+  eleventyConfig.addPassthroughCopy({ "src/assets/fonts": "assets/fonts" });
+
+  // Markdown configuration
+  let options = {
+    html: true,
+    breaks: true,
+    linkify: true,
+    typographer: true,
+  };
+  let opts = {
+    permalink: true,
+    permalinkClass: "direct-link",
+    permalinkSymbol: "#"
+  };
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+  };
+}
---
+++ src/posts/20250313-on-christian-politics.md
@@ -0,0 +1,13 @@
+---
+title: Christian Politics
+slug: christian-politics
+description: The hypocrisy of evangelical Christian politics in modern America.
+layout: layouts/single.njk
+date: 2025-03-13
+---
+
+Your politics and the way you vote is a type of fruit. When the leaders you helped put in office make decisions that allow millions to die, cause your friend’s business to go bankrupt, make the rich richer while making the poor more oppressed, and cause children to go hungry — what else can you assume than that fruit is rotten.
+
+And how can you be listening to the Holy Spirit when your ears are constantly filled with the lies of other men? No wonder you produce rotten fruit. You’ve mistake their voices for Christ’s and made Christ the scapegoat.
+
+How can you recognized deception when all you have known is lies? Should you keep on voting for people who lie to you before turning around and doing the work of the devil? You are complicit in their sin when you fail to recognize their actions--which are in plain sight, by the way--in favor of their deceptions.
